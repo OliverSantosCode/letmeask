973d447a0362d1c4681adb548f78d09efecd1881
Rodrigo Oliveira <oliversantoscode@gmail.com>
2021-07-29 12:44:05 -0300

    added: file Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import IlustrattionImg from "../assets/images/illustration.svg"
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+
+export function Home() {
+    return (
+        <div>
+            <aside>
+              <img src={IlustrattionImg} alt="Illustration symbolic questions and answers" />
+              <strong>Create live Q&amp;A rooms</strong>
+              <p>Ask your audience questions in real time</p>
+            </aside>
+            <main>
+                <div>
+                    <img src={logoImg} alt="Letmeask" />
+                    <button>
+                        <img src={googleIconImg} alt="Logo Google" />
+                        Create your room with Google
+                    </button>
+                    <div>Or enter a room</div>
+                    <form>
+                        <input 
+                         type="text" 
+                         placeholder="Enter room code"
+                        />
+                        <button type="submit">
+                            Entre in the room
+                        </button>
+                    </form>
+                    
+                </div>
+            </main>
+        </div>
+    );
+}
