a432a892bf27a96e4ccb10092149e325c93f7485
Rodrigo Oliveira <oliversantoscode@gmail.com>
2021-07-30 10:50:18 -0300

    modified: files AuthContext

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
 import { Button } from "../components/Button";
-import { AuthContext } from "../App";
+import { AuthContext } from "../contexts/AuthContext";
 
 import "../styles/auth.scss";
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,16 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import IlustrattionImg from "../assets/images/illustration.svg"
 import logoImg from "../assets/images/logo.svg";
 
 import { Button } from "../components/Button";
-import { AuthContext } from "../App";
+// import { AuthContext } from "../contexts/AuthContext"; 
 
 import "../styles/auth.scss";
 
 export function NewRoom() {
-    const { user } = useContext(AuthContext);
+    // const { user } = useContext(AuthContext);
 
     return (
         <div id="page-auth">
@@ -22,7 +22,6 @@ export function NewRoom() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <h1>{user?.name}</h1>
                     <h2>Create new room</h2>
                     <form>
                         <input 
