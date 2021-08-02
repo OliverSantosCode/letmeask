41c4d35fe41eac7466b0021badbb1d99173556a5
Rodrigo Oliveira <oliversantoscode@gmail.com>
2021-08-02 17:24:33 -0300

    modified: file - handleJoin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from "react-router-dom";
+import { FormEvent, useState } from "react";
 
 import IlustrattionImg from "../assets/images/illustration.svg"
 import logoImg from "../assets/images/logo.svg";
@@ -6,18 +7,37 @@ import googleIconImg from "../assets/images/google-icon.svg";
 
 import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
 import "../styles/auth.scss";
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [ roomCode, setRoomCode ] = useState('');
 
     async function handleCreateRoom() {
         if (!user) {
            await signInWithGoogle();
         }
         history.push("/rooms/new");
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists.');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
 
     return (
@@ -35,10 +55,12 @@ export function Home() {
                         Create your room with Google
                     </button>
                     <div className="separator">Or enter a room</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                          type="text" 
                          placeholder="Enter room code"
+                         onChange={event => setRoomCode(event.target.value)}
+                         value={roomCode}
                         />
                         <Button type="submit">
                             Entre in the room
