eb8825c6b1a71ec57561446905b699565561dad1
Rodrigo Oliveira <oliversantoscode@gmail.com>
2021-08-02 21:04:53 -0300

    modified: file - Room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,37 @@
+import logoImg from "../assets/images/logo.svg";
+
+import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+
+import "../styles/room.scss";
+
 export function Room() {
     return (
-        <h1>Sala</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <RoomCode code={"-Mg7I8c9Mr2cxo7VqkfB"}/>
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Room React</h1>
+                    <span>4 Questions</span>
+                </div>
+
+                <form>
+                    <textarea 
+                     placeholder="What do you want to ask?"
+                    />
+
+                    <div className="form-footer">
+                        <span>To submit a question, <button>please login</button>.</span>
+                        <Button type="submit">Submit question</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     );
 }
