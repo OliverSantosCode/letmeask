e4747ed261ff66a34e8c84c368e9a83b5e021ed0
Rodrigo Oliveira <oliversantoscode@gmail.com>
2021-08-10 21:00:41 -0300

    modified: files - like-button

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 export function useRoom(roomId: string) {
+    const { user } = useAuth();
+    const [questions, setQuestion] = useState<QuestionType[]>([]);
+    const [title, setTitle] = useState('');
 
     type FirebaseQuestions = Record<string, {
         author: {
@@ -11,6 +15,9 @@ export function useRoom(roomId: string) {
         content: string;
         isAnswered: boolean;
         isHighLighted: boolean;
+        likes: Record<string, {
+            authorId: string;
+        }>
     }>
 
     type QuestionType = {
@@ -22,10 +29,9 @@ export function useRoom(roomId: string) {
         content: string;
         isAnswered: boolean;
         isHighLighted: boolean;
+        likeCount: number;
+        likeId: string | undefined;
     }
-
-    const [questions, setQuestion] = useState<QuestionType[]>([]);
-    const [title, setTitle] = useState('');
 
     useEffect(() => {
         const roomRef = database.ref(`rooms/${roomId}`);
@@ -41,6 +47,8 @@ export function useRoom(roomId: string) {
                     author: value.author,
                     isHighLighted: value.isHighLighted,
                     isAnswered: value.isAnswered,
+                    likeCount: Object.values(value.likes ?? {}).length,
+                    likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
 
                 }
             })
@@ -48,7 +56,12 @@ export function useRoom(roomId: string) {
             setTitle(databaseRoom.title);
             setQuestion(parsedQuestions);
         })
-    }, [roomId]);
+
+        return () => {
+            roomRef.off('value');
+        }
+
+    }, [roomId, user?.id]);
 
     return { questions, title }
 }
